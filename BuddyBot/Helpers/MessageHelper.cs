9f69728a0398d455168568e338b0c8c027469cc4
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-04-30 20:14:31 +1200

    Remove unused usings in MessageHelper

--- BuddyBot/Helpers/MessageHelper.cs
+++ BuddyBot/Helpers/MessageHelper.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using System.Web;
-using Microsoft.Bot.Builder.Dialogs;
-using Microsoft.Bot.Connector;
 
 namespace BuddyBot.Helpers
 {
