20eefd4ea2db27e038cdb8098bc299f3bcc5c760
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-05-28 19:53:37 +1200

    add city domain model

---
+++ BuddyBot.Repository/Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuddyBot.Repository.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public Coordinate Coordinates { get; set; }
+
+        public class Coordinate
+        {
+            public double Longitude { get; set; }
+            public double latitude { get; set; }
+        }
+    }
+}
