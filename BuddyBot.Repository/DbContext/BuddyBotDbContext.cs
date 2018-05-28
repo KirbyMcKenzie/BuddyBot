09857cb5780ba3bc99620329eba663c7319a8666
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-05-28 20:27:19 +1200

    Add BuddyBotDbContext and DbContextFactory

---
+++ BuddyBot.Repository/DbContext/BuddyBotDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BuddyBot.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuddyBot.Repository.DbContext
+{
+    class BuddyBotDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public BuddyBotDbContext(DbContextOptions<BuddyBotDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>().ToTable("Athlete").HasKey(_ => _.Id);
+
+        }
+
+        public DbSet<City> Athletes { get; set; }
+    }
+}
