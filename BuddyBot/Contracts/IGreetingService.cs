72f7d8f62f4252088709f4c955d11b8cccdb1e58
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-06-07 20:41:30 +1200

    Add GreetingService shell

---
+++ BuddyBot/Contracts/IGreetingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BuddyBot.Contracts
+{
+    public interface IGreetingService
+    {
+        Task GetRandomGreeting();
+
+    }
+}
---
+++ BuddyBot/Services/GreetingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using BuddyBot.Contracts;
+
+namespace BuddyBot.Services
+{
+    public class GreetingService : IGreetingService
+    {
+        public async Task GetRandomGreeting()
+        {
+            await Task.Yield();
+        }
+
+    }
+}
