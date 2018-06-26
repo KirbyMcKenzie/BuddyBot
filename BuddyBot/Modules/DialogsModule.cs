fcc163e145a1489538f98d56d962667ac65f63a5
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-06-26 19:56:53 +1200

    Add basic DialogsModule class with load method

---
+++ BuddyBot/Modules/DialogsModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+
+namespace BuddyBot.Modules
+{
+    public class DialogsModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            
+        }
+    }
+}
