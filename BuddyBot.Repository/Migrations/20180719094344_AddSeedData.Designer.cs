3371adb61559194b7a1a543ba27937ff5220b004
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-07-19 21:45:09 +1200

    Add seed migration

--- BuddyBot.Repository/Migrations/20180719094344_AddSeedData.Designer.cs
+++ BuddyBot.Repository/Migrations/20180719094344_AddSeedData.Designer.cs
@@ -4,14 +4,16 @@ using BuddyBot.Repository.DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BuddyBot.Repository.Migrations
 {
     [DbContext(typeof(BuddyBotDbContext))]
-    partial class BuddyBotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180719094344_AddSeedData")]
+    partial class AddSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
