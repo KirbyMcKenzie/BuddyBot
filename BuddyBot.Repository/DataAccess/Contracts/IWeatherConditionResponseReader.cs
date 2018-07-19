84b57693d177eb044e1903f579b296222c608a27
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-07-20 09:02:53 +1200

    Add weatherConditionResponseReader class w/ GetResponseByCondition method

---
+++ BuddyBot.Repository/DataAccess/Contracts/IWeatherConditionResponseReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BuddyBot.Repository.Models;
+
+namespace BuddyBot.Repository.DataAccess.Contracts
+{
+    public interface IWeatherConditionResponseReader
+    {
+        Task<WeatherConditionResponse> GetResponseByCondition(string condition);
+    }
+}
