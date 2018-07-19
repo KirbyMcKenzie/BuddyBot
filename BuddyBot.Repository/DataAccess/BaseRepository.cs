f6df7987c97f7f2d032a70760f23887374aceb9f
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-07-20 08:53:42 +1200

    Add baseRepository for db access

---
+++ BuddyBot.Repository/DataAccess/BaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BuddyBot.Repository.DbContext;
+
+namespace BuddyBot.Repository.DataAccess
+{
+    public abstract class BaseRepository
+    {
+        protected BuddyBotDbContext _context;
+
+        public BaseRepository(BuddyBotDbContext context)
+        {
+            _context = context;
+        }
+
+        protected async Task<bool> SaveChangesAsync()
+        {
+            try
+            {
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // TODO - logging
+                return false;
+            }
+        } 
+    }
+
+}
