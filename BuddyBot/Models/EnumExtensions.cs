b0e478d2018d78005f2619a4d6bce9e8d7f13cf7
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-08-08 08:54:40 +1200

    Add BotDataService classes and extensions

---
+++ BuddyBot/Models/EnumExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace BuddyBot.Models
+{
+    public static class EnumExtensions
+    {
+        public static string GetFriendlyName(this DataStoreKey value)
+        {
+            return GetFriendlyNameFromAttribute(value);
+        }
+
+        public static string GetKey(this DataStoreKey value)
+        {
+            return value.ToString("G");
+        }
+
+        public static DataStoreEntryAttribute GetDataStoreEntry(this DataStoreKey value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DataStoreEntryAttribute[] entryAttributes =
+                (DataStoreEntryAttribute[])fi.GetCustomAttributes(typeof(DataStoreEntryAttribute), false);
+
+            if (entryAttributes.Length > 0)
+                return entryAttributes[0];
+            return null;
+        }
+        private static string GetFriendlyNameFromAttribute<T>(T value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DataStoreEntryAttribute[] entryAttributes =
+                (DataStoreEntryAttribute[])fi.GetCustomAttributes(typeof(DataStoreEntryAttribute), false);
+
+            if (entryAttributes.Length > 0)
+                return entryAttributes[0].FriendlyName;
+            return string.Empty;
+        }
+    }
+}
