10f205feccfb0167171012b2f0d2c6ebd5b04e58
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-08-07 17:39:51 +1200

    Add Settings classes for bot storage

---
+++ BuddyBot/Settings/AzureStorageSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace BuddyBot.Settings
+{
+    public class AzureStorageSettings : IAzureStorageSettings
+    {
+        public string ConnectionString { get; }
+        public string UserPreferencesDataTableName { get; }
+
+        public AzureStorageSettings()
+        {
+            ConnectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+            UserPreferencesDataTableName = ConfigurationManager.AppSettings["UserPreferencesDataTableName"];
+        }
+    }
+
+    
+}
