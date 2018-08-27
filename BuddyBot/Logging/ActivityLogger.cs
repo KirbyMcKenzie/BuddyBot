50819510dc60c6e13be27d355edaf1252f6faa02
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-08-27 19:29:06 +1200

    Add ActivityLogger

---
+++ BuddyBot/Logging/ActivityLogger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Bot.Builder.History;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BuddyBot.Logging
+{
+    public class ActivityLogger: IActivityLogger
+    {
+
+        public async Task LogAsync(IActivity activity)
+        {
+            Debug.WriteLine($"From:{activity.From.Id} - To:{activity.Recipient.Id} - Message:{activity.AsMessageActivity()?.Text}");
+
+            await Task.Yield();
+        }
+    }
+}
