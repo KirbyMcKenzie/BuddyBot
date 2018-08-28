b738fea7c80efdb1241e948dbb9aa3e6738d4eab
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-08-28 20:10:41 +1200

    Add serilog Azure table sotrage to ActivityLogger

--- BuddyBot/Logging/ActivityLogger.cs
+++ BuddyBot/Logging/ActivityLogger.cs
@@ -6,6 +6,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Serilog;
 
 namespace BuddyBot.Logging
 {
@@ -14,7 +17,21 @@ namespace BuddyBot.Logging
 
         public async Task LogAsync(IActivity activity)
         {
+            var storage = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("azureStorage:connectionString"));
+
+            var log = new LoggerConfiguration()
+                .WriteTo.AzureTableStorage(storage)
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            // TODO - come back to this
+            /*var log = new LoggerConfiguration() 
+                          .WriteTo.AzureTableStorageWithProperties(storage, storageTableName: "customName", writeInBatches: true, batchPostingLimit: 100, period: new System.TimeSpan(0, 0, 3)) 
+                          .CreateLogger(); 
+            */
+
             Debug.WriteLine($"From:{activity.From.Id} - To:{activity.Recipient.Id} - Message:{activity.AsMessageActivity()?.Text}");
+            log.Information($"From:{activity.From.Id} - To:{activity.Recipient.Id} - Message:{activity.AsMessageActivity()?.Text}");
 
             await Task.Yield();
         }
