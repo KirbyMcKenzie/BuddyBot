9aa4cd6e63c270ca5450bf71f2904f9ed024759c
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-08-30 08:52:52 +1200

    Remove uneeded logging config

--- BuddyBot/Logging/ActivityLogger.cs
+++ BuddyBot/Logging/ActivityLogger.cs
@@ -32,14 +32,11 @@ namespace BuddyBot.Logging
                 .MinimumLevel.Debug()
                 .CreateLogger();
 
-            // TODO - come back to this
-            /*var log = new LoggerConfiguration() 
-                          .WriteTo.AzureTableStorageWithProperties(storage, storageTableName: "customName", writeInBatches: true, batchPostingLimit: 100, period: new System.TimeSpan(0, 0, 3)) 
-                          .CreateLogger(); 
-            */
-
-            Debug.WriteLine($"From:{activity.From.Id} - To:{activity.Recipient.Id} - Message:{activity.AsMessageActivity()?.Text}");
-            log.Information($"From:{activity.From.Id} - To:{activity.Recipient.Id} - Message:{activity.AsMessageActivity()?.Text}");
+            if (activity.AsMessageActivity() != null)
+            {
+                Debug.WriteLine($"From:{activity.From.Id} - To:{activity.Recipient.Id} - Message:{activity.AsMessageActivity().Text}");
+                log.Information($"From:{activity.From.Id} - To:{activity.Recipient.Id} - Message:{activity.AsMessageActivity().Text}");
+            }
 
             await Task.Yield();
         }
