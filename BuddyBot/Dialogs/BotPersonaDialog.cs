92a3d5643f837f6cc64ed8889482d0273d8d17a0
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2018-11-23 11:38:10 +1300

    refactor BotPersonaDialog

--- BuddyBot/Dialogs/BotPersonaDialog.cs
+++ BuddyBot/Dialogs/BotPersonaDialog.cs
@@ -17,79 +17,83 @@ namespace BuddyBot.Dialogs
         private readonly IBotDataService _botDataService;
         private readonly IList<EntityRecommendation> _entities;
         private PersonalityChatPersona _preferredBotPersona;
-        private PersonalityChatPersona _heroCardSelectionPersona;
 
         public BotPersonaDialog(IBotDataService botDataService, IList<EntityRecommendation> entities, 
-            PersonalityChatPersona heroCardSelectionPersona)
+            PersonalityChatPersona preferredBotPersona)
         {
             SetField.NotNull(out _botDataService, nameof(botDataService), botDataService);
             _entities = entities;
-            _heroCardSelectionPersona = heroCardSelectionPersona;
+            _preferredBotPersona = preferredBotPersona;
         }
 
         public Task StartAsync(IDialogContext context)
         {
-            PersonalityChatPersona persona;
+            /**
+             * ----------------------------------------------------
+             *  Order of BotPersonaDialog Flow:
+             * ----------------------------------------------------
+             * 
+             * - Check if there is hero card selection, if yes, 
+             *   confirm and finish.
+             * 
+             * - Check if dialog has entities, extract, confirm and 
+             *   finish.
+             * 
+             * - Check if user has pre-saved persona, prompt to 
+             *   update, if yes,prompt options, confirm and finish. 
+             *   
+             * - Else, prompt user for option, confirm and finish.
+             **/
 
-            // If called from another dialog with chosen personality
-            if (_heroCardSelectionPersona != PersonalityChatPersona.None)
+            // Check if there is hero card selection
+            if (_preferredBotPersona != PersonalityChatPersona.None)
             {
-                PromptDialog.Confirm(context, ResumeAfterHeroCardChoiceConfirmation, $"So you'd like me to change my personality to {_heroCardSelectionPersona}?", $"Sorry I don't understand - try again! Should I change my personality to {_heroCardSelectionPersona}?");
+                PromptDialog.Confirm(context, ResumeAfterConfirmChosenPersona, 
+                    $"So you'd like me to change my personality to {_preferredBotPersona}?", 
+                    $"Sorry I don't understand - try again! Should I change my personality to {_preferredBotPersona}?");
+
                 return Task.CompletedTask;
             }
             // Else we can assume LUIS called the dialog
             else
             {
-                persona = _botDataService.GetPreferredBotPersona(context);
-
-                if (_entities != null)
+                if (_entities.Count > 0 || _entities != null)
                 {
-                    Enum.TryParse(MessageHelpers.ExtractEntityFromMessage("User.PreferredBotPersona", _entities), out PersonalityChatPersona parsedResult);
+                    Enum.TryParse(MessageHelpers.ExtractEntityFromMessage("User.PreferredBotPersona", _entities),
+                        out PersonalityChatPersona parsedResult);
+
                     _preferredBotPersona = parsedResult;
                 }
             }
 
-
+            // Check If LUIS found entity preference, otherwise continue
             if (_preferredBotPersona != PersonalityChatPersona.None)
             {
-                PromptDialog.Confirm(context, ResumeAfterPreferredPersonaConfirmation, $"So you'd like me to change my personality to {_preferredBotPersona}?", $"Sorry I don't understand - try again! Should I change my personality to {_preferredBotPersona}?");
+                PromptDialog.Confirm(context, ResumeAfterConfirmChosenPersona, 
+                    $"So you'd like me to change my personality to {_preferredBotPersona}?",
+                    $"Sorry I don't understand - try again! Should I change my personality to {_preferredBotPersona}?");
+
                 return Task.CompletedTask;
             }
 
-            if (persona != PersonalityChatPersona.None)
+            PersonalityChatPersona preSavedPersona = _botDataService.GetPreferredBotPersona(context);
+            // Check if user has pre-saved persona, otherwise continue
+            if (preSavedPersona != PersonalityChatPersona.None)
             {
-                PromptDialog.Confirm(context, ResumeAfterConfirmation, $"My persona is set to {persona}. Would you like to change it?", $"Sorry I don't understand - try again! Would you like to change my persona?");
+                PromptDialog.Confirm(context, ResumeAfterUpdateConfirmation, $"My persona is set to {preSavedPersona}. Would you like to change it?", $"Sorry I don't understand - try again! Would you like to change my persona?");
                 return Task.CompletedTask;
             }
 
-            // Could not determine preferred personality
-            // prompt user to pick
-            PromptDialog.Choice(context, ResumeAfterPersonaFilled,Enum.GetValues(typeof(PersonalityChatPersona)).Cast<PersonalityChatPersona>(),
-                        "What would you like my personality to be?");
+            // Could not determine preferred personality prompt user to choose
+            PromptDialog.Choice(context, ResumeAfterPromptDialogChoice,
+                Enum.GetValues(typeof(PersonalityChatPersona)).Cast<PersonalityChatPersona>(),
+                "What would you like my personality to be?");
+
             return Task.CompletedTask;
         }
 
-        private async Task ResumeAfterHeroCardChoiceConfirmation(IDialogContext context, IAwaitable<bool> result)
+        private async Task ResumeAfterConfirmChosenPersona(IDialogContext context, IAwaitable<bool> result)
         {
-            bool confirmation = await result;
-
-            switch (confirmation)
-            {
-                case true:
-                    _botDataService.SetPreferredBotPersona(context, _heroCardSelectionPersona);
-                    context.Done(_heroCardSelectionPersona.ToString());
-                    break;
-                default:
-                    PromptDialog.Choice(context, ResumeAfterPersonaFilled,
-                       Enum.GetValues(typeof(PersonalityChatPersona)).Cast<PersonalityChatPersona>(),
-                       "What would you like my personality to be?");
-                    break;
-            }
-        }
-
-        private async Task ResumeAfterPreferredPersonaConfirmation(IDialogContext context, IAwaitable<bool> result)
-        {
-
             bool confirmation = await result;
 
             switch (confirmation)
@@ -99,22 +103,21 @@ namespace BuddyBot.Dialogs
                     context.Done(_preferredBotPersona.ToString());
                     break;
                 default:
-                    PromptDialog.Choice(context, ResumeAfterPersonaFilled,
+                    PromptDialog.Choice(context, ResumeAfterPromptDialogChoice,
                        Enum.GetValues(typeof(PersonalityChatPersona)).Cast<PersonalityChatPersona>(),
                        "What would you like my personality to be?");
                     break;
             }
-           
         }
 
-        private async Task ResumeAfterConfirmation(IDialogContext context, IAwaitable<bool> result)
+        private async Task ResumeAfterUpdateConfirmation(IDialogContext context, IAwaitable<bool> result)
         {
             bool confirmation = await result;
 
             switch (confirmation)
             {
                 case true:
-                    PromptDialog.Choice(context, ResumeAfterPersonaFilled,
+                    PromptDialog.Choice(context, ResumeAfterPromptDialogChoice,
                         Enum.GetValues(typeof(PersonalityChatPersona)).Cast<PersonalityChatPersona>(),
                         "What would you like my personality to be?");
                     break;
@@ -124,13 +127,12 @@ namespace BuddyBot.Dialogs
             }
         }
 
-        private async Task ResumeAfterPersonaFilled(IDialogContext context, IAwaitable<PersonalityChatPersona> result)
+        private async Task ResumeAfterPromptDialogChoice(IDialogContext context, IAwaitable<PersonalityChatPersona> result)
         {
-            PersonalityChatPersona filledPersona = await result;
+            PersonalityChatPersona chosenPersona = await result;
 
-            _botDataService.SetPreferredBotPersona(context, filledPersona);
-            context.Done(filledPersona.ToString());
+            _botDataService.SetPreferredBotPersona(context, chosenPersona);
+            context.Done(chosenPersona.ToString());
         }
-       
     }
 }
