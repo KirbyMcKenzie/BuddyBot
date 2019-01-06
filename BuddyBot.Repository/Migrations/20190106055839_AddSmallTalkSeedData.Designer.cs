ecd7df5e3c6700763d598e10ccd96f1389660017
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2019-01-06 19:01:30 +1300

    Add all seed data into one migration

--- BuddyBot.Repository/Migrations/20190106055839_AddSmallTalkSeedData.Designer.cs
+++ BuddyBot.Repository/Migrations/20190106055839_AddSmallTalkSeedData.Designer.cs
@@ -4,14 +4,16 @@ using BuddyBot.Repository.DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BuddyBot.Repository.Migrations
 {
     [DbContext(typeof(BuddyBotDbContext))]
-    partial class BuddyBotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190106055839_AddSmallTalkSeedData")]
+    partial class AddSmallTalkSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
