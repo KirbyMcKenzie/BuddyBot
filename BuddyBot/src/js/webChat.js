2562a4245b2b976d5ac7321dc8c7b48cfb95c9a5
Kirby McKenzie <kirby.mckenzie@intergen.co.nz>
2019-02-13 17:50:58 +1100

    move js code to sperate files

---
+++ BuddyBot/src/js/webChat.js
@@ -0,0 +1,50 @@
+﻿function setupWebChat() {
+
+    const styleOptions = {
+        //General
+        suggestedActionBackground: '#2E5EFF',
+        suggestedActionBorder: 'none',
+        suggestedActionBorderRadius: 10,
+        suggestedActionTextColor: 'white',
+
+        // BuddyBot
+        bubbleBackground: '#E3E8F1',
+        bubbleTextColor: '#32353A',
+        bubbleBorderRadius: 10,
+        bubbleBorder: 'none',
+
+        // User
+        bubbleFromUserBackground: '#2E5EFF',
+        bubbleFromUserTextColor: 'white',
+        bubbleFromUserBorderRadius: 10,
+        bubbleFromUserBorder: 'none'
+    };
+
+    window.WebChat.renderWebChat({
+        directLine: window.WebChat.createDirectLine({ secret: 'GWLuPsYNedU.cwA.khE.I3LeUlKURhomRrIo2FoLPM7amVzG594CvWOczgxpC30' }),
+        userID: getUserId(),
+        styleOptions
+    },
+        document.getElementById('webchat'));
+}
+
+function getUserId() {
+
+    function generateUniqueID() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+
+    var userId = localStorage.getItem('userId');
+
+    if (userId === null || userId === undefined) {
+        userId = (generateUniqueID() + generateUniqueID() + "-" + generateUniqueID() + "-4"
+            + generateUniqueID().substr(0, 3) + "-" + generateUniqueID() + "-" + generateUniqueID()
+            + generateUniqueID() + generateUniqueID()).toLowerCase();
+
+        localStorage.setItem('userId', userId);
+    }
+
+    return userId;
+}
+
+setupWebChat();
